18736559a0b8fa798a7b0147178f2ed5032b5984
Sofia <sofia.oyola@upb.edu.co>
2021-08-25 21:34:16 -0500

    creacion del proyecto en visual

---
+++ seccion2_parcialScripting/seccion2_parcialScripting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace seccion2_parcialScripting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
